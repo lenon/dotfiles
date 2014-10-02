6ff580a425901b921da35ccafab28df339c6c8b9
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-02 19:59:57 -0300

    Initial version of mymac.sh

---
+++ mymac.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$(uname)" != "Darwin" ]; then
+  echo "This script only works on Mac OS X"
+  exit 0
+fi
+
+BREW_INSTALLER="https://raw.githubusercontent.com/Homebrew/install/master/install"
+BREW_PACKAGES="
+  ag
+  bash
+  caskroom/cask/brew-cask
+  git
+  python
+  rename
+  tree
+  vim
+"
+CASK_PACKAGES="
+  adium
+  dropbox
+  flux
+  iterm2
+  onepassword
+  spotify
+"
+
+if command -v brew >/dev/null 2>&1; then
+  echo "Homebrew already installed"
+else
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL "${BREW_INSTALLER}")"
+fi
+
+echo "Installing homebrew packages..."
+
+for package in $BREW_PACKAGES ; do
+  if brew list "$package" >/dev/null 2>&1; then
+    echo "$package already installed"
+  else
+    brew install "$package"
+  fi
+done
+
+echo "Installing cask packages..."
+
+for package in $CASK_PACKAGES ; do
+  if brew cask list "$package" >/dev/null 2>&1; then
+    echo "$package already installed"
+  else
+    brew cask install --appdir=/Applications "$package"
+  fi
+done
+
+brew cleanup
+
+echo "Done!"
+exit 1
