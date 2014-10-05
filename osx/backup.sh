a81f99733db7cef306745e9cdcc03e4ebadecd5e
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-05 05:34:42 -0300

    Include a way to backup .plist files.

---
+++ osx/backup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Performs a backup of all .plist specified in "plists" file.
+set -e
+
+while read domain; do
+  plutil -convert xml1 -o - "${HOME}/Library/Preferences/${domain}" | \
+    xmllint --format - > "./Library/Preferences/${domain}"
+done < plists
