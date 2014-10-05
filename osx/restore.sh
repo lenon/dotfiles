a2b3fe9957f6d3ab603139ba2b1243eea1d9e4f0
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-05 05:43:30 -0300

    Provide a way to restore all .plist files

---
+++ osx/restore.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Restores a .plist file converting it to the binary form.
+set -e
+
+while read domain; do
+  plutil -convert binary1 -o "${HOME}/Library/Preferences/${domain}" - \
+    < "./Library/Preferences/${domain}"
+done < plists
