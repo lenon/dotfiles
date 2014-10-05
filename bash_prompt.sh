9633afd0f69c00df435b3e15ea17d6d1caed4548
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-04 22:41:52 -0300

    Create bash_prompt.sh

---
+++ bash_prompt.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+DOTFILES_ROOT=$(dirname "${BASH_SOURCE[0]}")
+source "${DOTFILES_ROOT}/lib/colors.sh"
+source "${DOTFILES_ROOT}/lib/git.sh"
+
+ps1_prompt() {
+  local branch_name=""
+  local prompt=""
+
+  if is_a_git_repo; then
+    if ! is_dot_git; then
+      update_git_index
+      has_uncommitted_changes && prompt+="+"
+      has_unstaged_files && prompt+="!"
+      has_untracked_files && prompt+="?"
+      has_stashed_files && prompt+="$"
+    fi
+
+    [ -n "${prompt}" ] && prompt=" [${prompt}]"
+
+    printf " ${COLOR_BLUE}$(git_branch_name)${COLOR_YELLOW}${prompt}${COLOR_RESET}"
+  else
+    return
+  fi
+}
+
+set_exitcode_color() {
+  if [ "$?" = 0 ]; then
+    printf "${COLOR_WHITE}"
+  else
+    printf "${COLOR_RED}"
+  fi
+}
+
+PROMPT_COMMAND=set_exitcode_color
+
+PS1="\[\033]0;\$(basename "\\w")\007\]" # show basename of the current directory as window title
+PS1+="\w" # current working directory
+PS1+="\$(ps1_prompt)" # git repository information
+PS1+="${COLOR_WHITE} $ "
+PS1+="${COLOR_RESET}"
+
+PS2="\[${COLOR_YELLOW}\]→ \[${COLOR_RESET}\]"
+
+export PROMPT_COMMAND
+export PS1
+export PS2
