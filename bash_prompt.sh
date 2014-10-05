a720f0d43d7dce833dbf8abe4dd00d851d2df4b3
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-05 01:49:40 -0300

    Use printf arguments for better readability.

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -18,7 +18,7 @@ ps1_prompt() {
 
     [ -n "${prompt}" ] && prompt=" [${prompt}]"
 
-    printf " ${COLOR_BLUE}$(git_branch_name)${COLOR_YELLOW}${prompt}${COLOR_RESET}"
+    printf " ${COLOR_BLUE}%s${COLOR_YELLOW}%s${COLOR_RESET}" "$(git_branch_name)" "${prompt}"
   fi
 }
 
