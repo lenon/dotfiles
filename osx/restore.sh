d1d031509035168bbc4875ef52e5926d26aa092d
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-09 04:38:22 -0300

    Improves osx/restore description.

--- osx/restore.sh
+++ osx/restore.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Restores a .plist file converting it to the binary form.
+# This script restores all .plist files listed on config/plists by converting
+# the file to its binary form.
 set -e
 
 while read domain; do
