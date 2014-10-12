eca63332a290bf827de52bed2547512d92b9a262
Lenon Marcel <lenon.marcel@gmail.com>
2014-10-12 15:48:07 -0300

    Append ~/dotfiles/bin to PATH.

--- bash_profile.sh
+++ bash_profile.sh
@@ -2,6 +2,9 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/bash_prompt.sh"
 
+# Executable scripts
+PATH="${HOME}/dotfiles/bin:${PATH}"
+
 # Homebrew paths
 PATH="/usr/local/bin:${PATH}"
 PATH="/usr/local/sbin:${PATH}"
