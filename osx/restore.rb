796dfa9d8c68b6504b5fa41593baad64a9da8e9f
Lenon Marcel <lenon.marcel@gmail.com>
2015-12-01 23:26:28 -0200

    Append __dir__ when reading files to avoid problems when running these scripts from other directories

--- osx/restore.rb
+++ osx/restore.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require_relative 'plist'
 
-plists = File.readlines('plists').map(&:strip)
+plists = File.readlines(File.join(__dir__, 'plists')).map(&:strip)
 plists.each do |plist|
   Plist.new(plist).restore
 end
