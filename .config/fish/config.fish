f3cac31cb3f7b982c0c401945da1077f96b6d31d
Lenon Marcel <lenon.marcel@gmail.com>
2015-12-04 01:21:14 -0200

    Change some fish colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,7 @@
 set --export EDITOR vim
 set --export PATH /usr/local/bin $PATH
+
+set fish_color_command white
+set fish_color_autosuggestion yellow
+set fish_pager_color_description yellow
+set fish_color_param white
