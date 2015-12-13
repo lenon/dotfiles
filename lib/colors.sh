ad786260a4a310db9cf27467fbfcbae2876d733b
Lenon Marcel <lenon.marcel@gmail.com>
2015-12-13 02:02:35 -0200

    git-bump was turned into a gem

--- lib/colors.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-COLOR_WHITE=$(tput setaf 7)
-COLOR_RED=$(tput setaf 1)
-COLOR_GREEN=$(tput setaf 2)
-COLOR_BLUE=$(tput setaf 4)
-COLOR_YELLOW=$(tput setaf 3)
-COLOR_RESET=$(tput sgr0)
