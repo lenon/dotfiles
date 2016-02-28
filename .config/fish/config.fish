add84a8ac6245bc072c8dd0fb0fb3934667306b1
Lenon Marcel <lenon.marcel@gmail.com>
2016-02-27 21:21:26 -0300

    Initialize rbenv when starting a fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,5 @@ set fish_color_command white
 set fish_color_autosuggestion yellow
 set fish_pager_color_description yellow
 set fish_color_param white
+
+status --is-interactive; and . (rbenv init -|psub)
