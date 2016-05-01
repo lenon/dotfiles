d488118ddb633fd60bdeb6d30c0788e466c16a93
Lenon Marcel <lenon.marcel@gmail.com>
2016-04-30 23:10:00 -0300

    Update fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,47 @@
+set --local color_blue 005b96
+set --local color_green a2d27b
+set --local color_purple a0a0ff
+set --local color_red feb3b3
+set --local color_white ffffff
+set --local color_yellow fdf498
+
+# prompt colors
+set --global fish_color_autosuggestion $color_green
+set --global fish_color_command $color_white
+set --global fish_color_comment $color_purple
+set --global fish_color_cwd $color_white
+set --global fish_color_end $color_purple
+set --global fish_color_error $color_red
+set --global fish_color_escape $color_purple
+set --global fish_color_match $color_yellow
+set --global fish_color_normal $color_white
+set --global fish_color_operator $color_yellow
+set --global fish_color_param $color_white
+set --global fish_color_redirection $color_purple
+set --global fish_color_search_match --background=$color_blue
+
+# pager colors
+set --global fish_pager_color_completion $color_white
+set --global fish_pager_color_prefix $color_green
+set --global fish_pager_color_progress $color_yellow
+
+# git prompt settings
+set --global __fish_git_prompt_char_cleanstate '✓'
+set --global __fish_git_prompt_char_dirtystate "*"
+set --global __fish_git_prompt_char_stagedstate '±'
+set --global __fish_git_prompt_char_stateseparator ' '
+set --global __fish_git_prompt_show_informative_status 1
+set --global __fish_git_prompt_showcolorhints 1
+
+# hostname cache
+if not set --query __fish_prompt_hostname
+  set --global __fish_prompt_hostname (hostname | cut -d . -f 1)
+end
+
+# other stuff
 set --export EDITOR vim
 set --export PATH /usr/local/bin $PATH
 set --export PATH /usr/local/sbin $PATH
-
-set fish_color_command white
-set fish_color_autosuggestion yellow
-set fish_pager_color_description yellow
-set fish_color_param white
 
 status --is-interactive; and . (rbenv init -|psub)
 
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,45 +1,11 @@
 function fish_prompt
-  set -l last_status $status
-  set -l status_indicator " "
-
-  if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname | cut -d . -f 1)
+  if test $status -eq 0
+    set_color $fish_color_cwd
+  else
+    set_color $fish_color_error
   end
 
-  #
-  # settings for __fish_git_prompt
-  #
+  printf '%s@%s%s> ' (prompt_pwd) $__fish_prompt_hostname (__fish_git_prompt)
 
-  if not set -q __fish_git_prompt_show_informative_status
-    set -g __fish_git_prompt_show_informative_status 1
-  end
-
-  if not set -q __fish_git_prompt_showcolorhints
-    set -g __fish_git_prompt_showcolorhints 1
-  end
-
-  if not set -q __fish_git_prompt_char_stagedstate
-    set -g __fish_git_prompt_char_stagedstate '+'
-  end
-
-  if not set -q __fish_git_prompt_char_dirtystate
-    set -g __fish_git_prompt_char_dirtystate "*"
-  end
-
-  if not set -q __fish_git_prompt_char_untrackedfiles
-    set -g __fish_git_prompt_char_untrackedfiles 'u'
-  end
-
-  if not set -q __fish_git_prompt_char_upstream_ahead
-    set -g __fish_git_prompt_char_upstream_ahead '▴'
-  end
-
-  # put an exclamation mark "!" into the prompt if the last command
-  # exited with an error
-  if test $last_status -ne 0
-    set status_indicator "!"
-  end
-
-  echo -n -s (prompt_pwd) $normal "@" $__fish_prompt_hostname \
-    (__fish_git_prompt) $status_indicator "> "
+  set_color normal
 end
