4de8e2a9cdbadfab5c5f5c69ca38b3e60e46be2a
Lenon Marcel <lenon.marcel@gmail.com>
2016-05-30 05:11:17 -0300

    Remove old files

--- osx/backup.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-require_relative 'lib/plist'
-
-plists = File.readlines(File.join(__dir__, 'config/plists')).map(&:strip)
-plists.each do |plist|
-  Plist.new(plist).copy
-end
--- osx/lib/os.rb
+++
@@ -1,9 +0,0 @@
-# OS helpers.
-module OS
-  module_function
-
-  # Returns false if the current OS is not Mac OS X.
-  def mac?
-    RUBY_PLATFORM.downcase.include? 'darwin'
-  end
-end
