f14790a71dfc194e8e523e48c9d9e076b6e0c3ce
Lenon Marcel <lenon.marcel@gmail.com>
2016-10-05 23:42:10 -0300

    Include new git aliases

--- home/fish/.config/fish/aliases.fish
+++ home/fish/.config/fish/aliases.fish
@@ -1,5 +1,8 @@
 # git aliases
 alias g   'git'
-alias gs  'git status'
+alias ga  'git add'
+alias gc  'git commit'
 alias gd  'git diff'
 alias gds 'git diff --staged'
+alias gp  'git push'
+alias gs  'git status'
