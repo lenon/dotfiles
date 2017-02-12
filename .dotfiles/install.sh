300837ada8dd0808e8890df5f00a26efa7b6cc54
Lenon Marcel <lenon.marcel@gmail.com>
2017-02-12 02:43:26 -0200

    Rename playbook

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -18,4 +18,4 @@ if ! command -v ansible-playbook > /dev/null; then
   brew install ansible
 fi
 
-ansible-playbook -i localhost, --ask-sudo-pass playbooks/dotfiles.yml
+ansible-playbook -i localhost, --ask-sudo-pass macos.yml
